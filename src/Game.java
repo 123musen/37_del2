b29fb32528b0f06dcfa3cd7aede895d81f125679
Meetopia <itsmeehansen@hotmail.com>
2019-10-28 12:48:59 +0100

    Added extra turn Added default to switch case Fixed output

--- src/Game.java
+++ src/Game.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 //TODO Explanation for game, Player turn
 
 public class Game {
+
     public void Round() {
         Scanner scan = new Scanner(System.in);
 
@@ -15,77 +16,73 @@ public class Game {
         Player p2 = new Player(scan.nextLine());
 
 
-        System.out.println("Press enter to start");
-        while (!scan.nextLine().equalsIgnoreCase("Q")) {
-            System.out.println("Player 1. Press enter for turn");
-            System.out.println(Turn(p1, d1, d2));
-            System.out.println("Player 2. Press enter for turn");
+        while (true) {
+            System.out.print("\nPlayer 1. Press enter for turn");
+            if (scan.nextLine().equalsIgnoreCase("Q")){
+                return;
+            }
+            Turn(p1, d1, d2);
+            System.out.print("\nPlayer 2. Press enter for turn");
             scan.nextLine();
-            System.out.println(Turn(p2, d1, d2));
-
+            Turn(p2, d1, d2);
         }
     }
-    private String Turn(Player p, Dice d1, Dice d2) {
-        d1.rollDice();
-        d2.rollDice();
-        switch (d1.getEyes() + d2.getEyes()) {
+    private void Turn(Player player, Dice dice1, Dice dice2) {
+        dice1.rollDice();
+        dice2.rollDice();
+        Scanner scan = new Scanner(System.in);
+        switch (dice1.getEyes() + dice2.getEyes()) {
             //p1 is an object that calls the class "Account" and calls the method "changeCoins()"
             case 2:
-                p.getAccount().changeCoins(250);
-                System.out.print("Tower +250. Total coins: " + p.getAccount().getCoins());
+                player.getAccount().changeCoins(250);
+                System.out.println("Tower +250. Total coins: " + player.getAccount().getCoins());
                 break;
-
             case 3:
-                p.getAccount().changeCoins(-100);
-                System.out.print("Crater -100. Total coins: " + p.getAccount().getCoins());
+                player.getAccount().changeCoins(-100);
+                System.out.println("Crater -100. Total coins: " + player.getAccount().getCoins());
                 break;
-
             case 4:
-                p.getAccount().changeCoins(100);
-                System.out.print("Palace Gates +100. Total coins: " + p.getAccount().getCoins());
+                player.getAccount().changeCoins(100);
+                System.out.println("Palace Gates +100. Total coins: " + player.getAccount().getCoins());
                 break;
-
             case 5:
-                p.getAccount().changeCoins(-20);
-                System.out.print("Cold Desert -20. Total coins: " + p.getAccount().getCoins());
+                player.getAccount().changeCoins(-20);
+                System.out.println("Cold Desert -20. Total coins: " + player.getAccount().getCoins());
                 break;
-
             case 6:
-                p.getAccount().changeCoins(180);
-                System.out.print("Walled city +180. Total coins: " + p.getAccount().getCoins());
+                player.getAccount().changeCoins(180);
+                System.out.println("Walled city +180. Total coins: " + player.getAccount().getCoins());
                 break;
-
             case 7:
-                p.getAccount().changeCoins(0);
-                System.out.print("Monastary 0. Total coins: " + p.getAccount().getCoins());
+                player.getAccount().changeCoins(0);
+                System.out.println("Monastary 0. Total coins: " + player.getAccount().getCoins());
                 break;
-
             case 8:
-                p.getAccount().changeCoins(-70);
-                System.out.print("Black Cave -70. Total coins: " + p.getAccount().getCoins());
+                player.getAccount().changeCoins(-70);
+                System.out.println("Black Cave -70. Total coins: " + player.getAccount().getCoins());
                 break;
-
             case 9:
-                p.getAccount().changeCoins(60);
-                System.out.print("Huts in the Mountain +60. Total coins: " + p.getAccount().getCoins());
+                player.getAccount().changeCoins(60);
+                System.out.println("Huts in the Mountain +60. Total coins: " + player.getAccount().getCoins());
                 break;
-
             case 10:
-                p.getAccount().changeCoins(-80);
-                System.out.println("The Werewolf-Wall -80. Total coins: " + p.getAccount().getCoins());
-                System.out.print("roll again");
+                player.getAccount().changeCoins(-80);
+                System.out.println("The Werewolf-Wall -80. Total coins: " + player.getAccount().getCoins());
+                System.out.print("roll again (press enter)");
+                scan.nextLine();
+                Turn(player, dice1, dice2);
                 break;
-
             case 11:
-                p.getAccount().changeCoins(-50);
-                System.out.print("The Pit -50. Total coins: " + p.getAccount().getCoins());
+                player.getAccount().changeCoins(-50);
+                System.out.println("The Pit -50. Total coins: " + player.getAccount().getCoins());
                 break;
-
             case 12:
-                p.getAccount().changeCoins(650);
-                System.out.print("Goldmine +650. Total coins: " + p.getAccount().getCoins());
+                player.getAccount().changeCoins(650);
+                System.out.println("Goldmine +650. Total coins: " + player.getAccount().getCoins());
                 break;
+            default:
+                System.out.println("Error");
+
         }
-        return "";
     }
 }
