c9bc6f2933884b6404fa27cc45345c8c5b54baf5
Silas Rindorf <s195474@student.dtu.dk>
2019-10-29 13:13:56 +0100

    Added swapDices

--- src/Player.java
+++ src/Player.java
@@ -10,13 +10,27 @@ public class Player {
 
     private String name;
     private Account account;
-
+    private Dice[] dice = new Dice[2];
 
     //constructor
     public Player(String name){
             this.name = name;
             //creating an account from the Class Account
             account = new Account();
+            dice[0] = new Dice(6);
+            dice[1] = new Dice(6);
+    }
+
+    public void swapDices(Player player){
+        Dice[] temp = this.getDice();
+        this.setDice(player.getDice());
+        player.setDice(temp);
+    }
+    public Dice[] getDice(){
+        return dice;
+    }
+    public void setDice(Dice[] dice){
+        this.dice = dice;
     }
 
     //method
