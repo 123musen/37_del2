ffca9b394314ce622ea585a58e2eb47afd4b3295
Silas Rindorf <s195474@student.dtu.dk>
2019-11-01 08:54:18 +0100

    Added Account

---
+++ src/AccountTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AccountTest {
+
+    @Test
+    public void setCoins() {
+        Account account1 = new Account();
+        account1.setCoins(2);
+        assertEquals(2,account1.getCoins());
+    }
+
+    @Test
+    public void changeCoins() {
+        Account account2 = new Account();
+        account2.changeCoins(-2000);
+        assertEquals(0,account2.getCoins());
+    }
+
+    @Test
+    public void getCoins() {
+        Account account3 = new Account();
+        assertEquals(1000,account3.getCoins());
+    }
+}
